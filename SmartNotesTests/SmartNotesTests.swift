e0fc5b15e5dccb54959926febbd33bcbbf733203
Henry Cooper <henrycooper@sandiego.edu>
2025-02-25 15:50:04 -0800

    Initial Commit

---
+++ SmartNotesTests/SmartNotesTests.swift
@@ -0,0 +1,16 @@
+//
+//  SmartNotesTests.swift
+//  SmartNotesTests
+//
+//  Created by Henry Cooper on 2/25/25.
+//
+
+import Testing
+
+struct SmartNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
